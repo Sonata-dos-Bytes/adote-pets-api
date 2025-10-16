bcadc2adb3fe42ea42807ac9caa3c89494d47eb3
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2025-10-16 16:21:45 -0300

    feat: refatorar lógica de cadastro para usar UserRepository e melhorar a estrutura do código

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
-import { Request, Response, NextFunction, response } from 'express';
-import { logger } from '../config/logger.js';
+import { Request, Response, NextFunction } from 'express';
 import { success } from 'src/utils/response.js';
 import { RegisterRequest, registerSchema } from 'src/schemas/auth.schema.js';
-import { prismaClient } from '@config/database.js';
 import { CustomError } from 'src/exceptions/customError.js';
 import { HTTP_STATUS } from 'src/utils/constants.js';
 import { hashPassword } from 'src/utils/encryption.js';
+import UserRepository from 'src/repository/user.repository.js';
 
 export async function signup(
   req: Request,
@@ -17,20 +16,15 @@ export async function signup(
     const files = req.files as Express.Multer.File[];
     const avatar = files?.find((f) => f.fieldname === 'avatar');
 
-    let user = await prismaClient.user.findFirst({where: {email: registerData.email}})
+    let user = await UserRepository.findByEmail(registerData.email);
     if (user) {
       throw new CustomError("User already exists!", HTTP_STATUS.NOT_FOUND);
     }
 
-    user = await prismaClient.user.create({
-      data: {
-        name: registerData.name,
-        email: registerData.email,
-        password: await hashPassword(registerData.password),
-        phone: registerData.phone || null,
-        avatar: null,
-      }
-    })
+    user = await UserRepository.createUser({
+      ...registerData,
+      password: await hashPassword(registerData.password),
+    });
 
     return res.json(success('User registered successfully', user));
   } catch (err) {
---
+++ src/repository/user.repository.ts
@@ -0,0 +1,52 @@
+import { prismaClient } from "@config/database";
+import { RegisterRequest } from "src/schemas/auth.schema";
+
+export default class UserRepository {
+  static async findAll() {
+    return await prismaClient.user.findMany();
+  }
+
+  static async findByEmail(email: string) {
+    return await prismaClient.user.findUnique({ where: { email } });
+  }
+
+  static async findById(id: number) {
+    return await prismaClient.user.findUnique({ where: { id } });
+  }
+
+  static async createUser(data: RegisterRequest) {
+    let avatarUrl = null;
+
+    return await prismaClient.user.create({
+      data: {
+        name: data.name!,
+        email: data.email!,
+        password: data.password!,
+        phone: data.phone || null,
+        avatar: avatarUrl,
+      },
+    });
+  }
+
+  static async updateUser(id: number, data: RegisterRequest) {
+
+    const updateData: any = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      phone: data.phone,
+      avatar: data.avatar,
+    };
+
+    return await prismaClient.user.update({
+      where: { id },
+      data: updateData,
+    });
+  }
+
+  static async deleteUser(id: number) {
+    return await prismaClient.user.delete({
+      where: { id },
+    });
+  }
+}
